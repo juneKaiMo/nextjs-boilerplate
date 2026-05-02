7528be4dbef0f9917b672b707c1c9aa7d794dee7
k7mo <eio@korea.com>
2026-05-02 15:52:25 +0900

    fix: 수정된 환영 메시지 텍스트

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <h1>방갑다 , 새롭게 프로그래밍 배워봅시다 하하하하2</h1>
+      <h1>방갑다 , 새롭게 프로그래밍 배워봅시다 하하하하3</h1>
     </div>
   );
 }
